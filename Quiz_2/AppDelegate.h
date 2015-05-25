6c5af0ca434131cbffa8f63ba60407525200aed9
Qiushi Li <qius.li3@gmail.com>
2015-05-25 02:44:50 -0500

    Initial Commit

---
+++ Quiz_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz_2
+//
+//  Created by Qiushi Li on 5/25/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz_2
+//
+//  Created by Qiushi Li on 5/25/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
